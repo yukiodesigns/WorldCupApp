3ada28de290c844964e53218fc0320d0b7217f03
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-21 08:10:20 +0300

    Added onclick

--- app/src/main/java/com/example/worldcup/WrldAdapter.kt
+++ app/src/main/java/com/example/worldcup/WrldAdapter.kt
@@ -1,11 +1,18 @@
 package com.example.worldcup
 
 import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class WrldAdapter(private var activity: Activity, private var items: ArrayList<CountryModel> ): BaseAdapter {
+class WrldAdapter(private var activity: Activity, private var items: ArrayList<CountryModel> ):
+    BaseAdapter() {
     override fun getCount(): Int {
         return items.size
     }
@@ -20,8 +27,47 @@ class WrldAdapter(private var activity: Activity, private var items: ArrayList<C
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         //Used in adapter to generate items view for a list view
+        val view: View?
+        val viewHolder: ViewHolder
 
+        if (convertView == null) {
+            val inflater =
+                activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = inflater.inflate(R.layout.list_item_layout, null)
+            viewHolder = ViewHolder(view)
+            view?.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+        var countries = items[position]
+        viewHolder.txtName?.text = countries.name
+        viewHolder.txtCupWin?.text = countries.cupWins
+        viewHolder.flagImg?.setImageResource(countries.flag)
+
+        view?.setOnClickListener(){
+            Toast.makeText(activity,
+                "You Choose: ${countries.name}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        return view as View
     }
 
+    private class ViewHolder(row: View?) {
+        var txtName: TextView? = null
+        var txtCupWin: TextView? = null
+        var flagImg: ImageView? = null
 
+        init {
+            this.txtName = row?.findViewById(R.id.country)
+            this.txtCupWin = row?.findViewById(R.id.wins)
+            this.flagImg = row?.findViewById(R.id.imageView)
+        }
+
+    }
 }
+
+
